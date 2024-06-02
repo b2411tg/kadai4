e929f705f1458a97d8426a45a7c486892c4c46e3
TAKAHIRO GOTO <b2411tg@aiit.ac.jp>
2024-06-02 09:48:09 +0900

    異常系で出力値誤記修正

--- gcd_test.sh
+++ gcd_test.sh
@@ -35,8 +35,8 @@ diff /tmp/$$-ans /tmp/$$-result || ERROR_EXIT "result err" # 失敗
 rm -f /tmp/$$-*
 
 # 正常系　引き数１と引き数２をマイナスで起動した時
-./greatest_common_divisor.sh 248 -528 > /tmp/$$-result
-echo "248 と -528 の最大公約数は 8 です。" > /tmp/$$-ans
+./greatest_common_divisor.sh -248 -528 > /tmp/$$-result
+echo "-248 と -528 の最大公約数は 8 です。" > /tmp/$$-ans
 diff /tmp/$$-ans /tmp/$$-result || ERROR_EXIT "result err" # 失敗
 rm -f /tmp/$$-*
 
@@ -59,7 +59,19 @@ diff /tmp/$$-ans /tmp/$$-result || ERROR_EXIT "result err" # 失敗
 rm -f /tmp/$$-*
 
 # 異常系　引き数を無しで起動した時
-./greatest_common_divisor.sh 248 > /tmp/$$-result
+./greatest_common_divisor.sh > /tmp/$$-result
+echo "入力エラー！ 引き数がないか、数字以外の文字が入力されました。" > /tmp/$$-ans
+diff /tmp/$$-ans /tmp/$$-result || ERROR_EXIT "result err" # 失敗
+rm -f /tmp/$$-*
+
+# 異常系　引き数１を数字以外で起動した時
+./greatest_common_divisor.sh test 248 > /tmp/$$-result
+echo "入力エラー！ 引き数がないか、数字以外の文字が入力されました。" > /tmp/$$-ans
+diff /tmp/$$-ans /tmp/$$-result || ERROR_EXIT "result err" # 失敗
+rm -f /tmp/$$-*
+
+# 異常系　引き数２を数字以外で起動した時
+./greatest_common_divisor.sh 248 test > /tmp/$$-result
 echo "入力エラー！ 引き数がないか、数字以外の文字が入力されました。" > /tmp/$$-ans
 diff /tmp/$$-ans /tmp/$$-result || ERROR_EXIT "result err" # 失敗
 rm -f /tmp/$$-*
